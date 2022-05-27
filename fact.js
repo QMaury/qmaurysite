fb265a3d235ac65399eafb31506563e42cf83b36
QMaury <octaviomaury1+1@gmail.com>
2022-05-27 07:38:33 -0700

    eugh

---
+++ fact.js
@@ -0,0 +1,7 @@
+function fact(){
+        var now = new Date()
+        if (0==now.getHours() && 0==now.getMinutes()){
+                document.write(quux);
+        }
+        setTimeout(timer_passage, 1000)//exec myself after 60000 ms
+}
